adef1caeda1c1db2cf7b065430cb6aff016cc754
Divya Nair <dnair@ucdavis.edu>
2021-06-01 08:34:13 -0700

    js updated

--- des157/final/proto/script.js
+++ des157/final/proto/script.js
@@ -1,5 +1,59 @@
 (function () {
 
-    // 'use strict';
-    // The JS is not included in my prototype version
+    'use strict';
+    alert("hello");
+    const holes = document.querySelectorAll('.hole');
+  const scoreBoard = document.querySelector('.score');
+  const moles = document.querySelectorAll('.mole');
+  let lastHole;
+  let timeUp = false;
+  let score = 0;
+  // Create new audio objects
+  const clicksound = new Audio('media/click-sound.mp3');
+
+  function randomTime(min, max) {
+    return Math.round(Math.random() * (max-min) + min);
+  }
+
+  function randomHole(holes){
+    const idx = Math.floor(Math.random() * holes.length);
+    const hole = holes[idx];
+    if(hole === lastHole) {
+      console.log("Ah nah that's the same one bud");
+      return randomHole(holes);
+    }
+    lastHole = hole;
+    return hole;
+  }
+
+  function peep(){
+    const time = randomTime(500, 1500);
+    const hole = randomHole(holes);
+    hole.classList.add("up");
+
+    setTimeout(() => {
+      hole.classList.remove('up');
+      if(!timeUp) peep();
+    }, time)
+}
+
+document.querySelector('.btn').addEventListener("click", startGame);
+
+function startGame() {
+  scoreBoard.textContent = 0;
+  timeUp = false;
+  score = 0;
+  peep();
+  setTimeout(() => timeUp = true, 20000);
+}
+
+function bonk(e) {
+  if(!e.isTrusted) return; //cheaters
+  score++;
+  this.classList.remove('up');
+  scoreBoard.textContent = score;
+  clicksound.play();
+}
+
+moles.forEach(mole => mole.addEventListener("click", bonk));
 })();
